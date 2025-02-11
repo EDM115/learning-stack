b93c845af63fa72a7a522b60e986fe38a430505d
EDM115 <dev@edm115.eu.org>
2025-02-11 02:22:28 +0100

    feat: super basic layout

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document"
 export default function Document() {
   return (
     <Html lang="fr">
-      <Head />
+      <Head>
+        {/* <script src="https://unpkg.com/react-scan/dist/auto.global.js" /> */}
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
