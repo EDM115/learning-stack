7c09effe924c7255adeef3e2d17b699a41884f72
EDM115 <dev@edm115.eu.org>
2025-02-11 01:29:44 +0100

    chore: basic logos, removed most layout, formatted code

---
+++ frontend/utils/fonts.ts
@@ -0,0 +1,32 @@
+import {
+  Comfortaa,
+  Fira_Code,
+  Inter,
+  Nunito,
+  Nunito_Sans
+} from "next/font/google"
+
+export const comfortaa = Comfortaa({
+  variable: "--font-comfortaa",
+  subsets: ["latin"]
+})
+
+export const fira_code = Fira_Code({
+  variable: "--font-fira-code",
+  subsets: ["latin"]
+})
+
+export const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"]
+})
+
+export const nunito = Nunito({
+  variable: "--font-nunito",
+  subsets: ["latin"]
+})
+
+export const nunito_sans = Nunito_Sans({
+  variable: "--font-nunito-sans",
+  subsets: ["latin"]
+})
